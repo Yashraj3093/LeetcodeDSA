19c42ae26ffefb4a8c69f5e8b681b10bd366b369
Yashraj Singh <115545111+Yashraj3093@users.noreply.github.com>
2026-01-19 19:01:56 +0530

    Time: 184 ms (81.70%) | Memory: 26.4 MB (22.77%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,25 +1,33 @@
 class Solution:
     def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        m, n = len(mat), len(mat[0])
-        P = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                P[i][j] = (P[i - 1][j] + P[i][j - 1] - P[i - 1][j - 1] + mat[i - 1][j - 1])
+        n = len(mat)
+        m = len(mat[0])
+        ps = [[0] * m for _ in range(n)]
 
-        def getRect(x1, y1, x2, y2):
-            return P[x2][y2] - P[x1 - 1][y2] - P[x2][y1 - 1] + P[x1 - 1][y1 - 1]
-
-        l, r, answer = 1, min(m,n), 0
-        while l <= r:
-            mid = (l + r) // 2
-            find = any(
-                getRect(i, j, i + mid - 1, j + mid - 1) <= threshold
-                for i in range(1, m - mid + 2)
-                for j in range(1, n - mid + 2)
+        for i in range(n):
+            for j in range(m):
+                ps[i][j] = (
+                    mat[i][j]
+                    + (ps[i - 1][j] if i > 0 else 0)
+                    + (ps[i][j - 1] if j > 0 else 0)
+                    - (ps[i - 1][j - 1] if i > 0 and j > 0 else 0)
+                )
+        
+        def getSum(i, j, size):
+            return(
+                ps[i + size][j + size]
+                - (ps[i - 1][j + size] if i > 0 and j + size < m else 0)
+                - (ps[i + size][j - 1] if i + size < n and j > 0 else 0)
+                + (ps[i - 1][j - 1]if i > 0 and j > 0 else 0)
             )
-            if find:
-                answer = mid
-                l = mid + 1
-            else:
-                r = mid - 1
-        return answer
+
+        result = 0
+
+        for i in range(n):
+            for j in range(m):
+                for k in range(result, min(n - i, m - j)):
+                    current = getSum(i, j, k)
+                    if current <= threshold: result = k + 1
+                    else: break
+
+        return result
