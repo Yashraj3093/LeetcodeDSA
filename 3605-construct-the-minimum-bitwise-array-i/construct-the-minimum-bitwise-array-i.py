9779c29b09d0bab0bcc4bdc6dd164f8aa250961e
Yashraj Singh <115545111+Yashraj3093@users.noreply.github.com>
2026-01-20 15:27:24 +0530

    Time: 143 ms (23.53%) | Memory: 19.4 MB (15.44%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -1,12 +1,25 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        # result = []
+        # for num in nums:
+        #     original = num 
+        #     candidate = -1
+        #     for j in range(1, original):
+        #         if (j | (j + 1)) == original:
+        #             candidate = j
+        #             break
+        #     result.append(candidate)
+        # return result 
+
+        n = len(nums)
         result = []
-        for num in nums:
-            original = num 
-            candidate = -1
-            for j in range(1, original):
-                if (j | (j + 1)) == original:
-                    candidate = j
+
+        for i in range(n):
+            current = -1
+            for c in range(nums[i]):
+                if c | (c + 1) == nums[i]:
+                    current = c
                     break
-            result.append(candidate)
-        return result 
+            result.append(current)
+        
+        return result
