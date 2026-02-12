8a002e4243f30e1cbd76fde51f0c4f04e08c4f42
Yashraj Singh <115545111+Yashraj3093@users.noreply.github.com>
2026-02-12 16:35:10 +0530

    Time: 2663 ms (54.64%) | Memory: 19.4 MB (20.27%) - LeetSync

---
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        n = len(s)
+        result = 0
+        for i in range(n):
+            cnt = defaultdict(int)
+            for j in range(i, n):
+                cnt[s[j]] += 1
+                if len(set(cnt.values())) == 1:
+                    result = max(result, j - i + 1)
+
+        return result  
